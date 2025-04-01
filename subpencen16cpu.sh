c48f18d07b090bb7b7f0156a9e12c1978474c877
kevinpinoarias <kevin.pino.arias@outlook.com>
2025-04-01 22:15:31 +0100

    Shell upload did not work
    All the scripts, named depending on how many cpus requested.

---
+++ subpencen16cpu.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+# Run in the standard partition (queue)
+#SBATCH --partition=teaching
+#
+# Specify project account
+#SBATCH --account=teaching
+# No. of tasks required (max. of 16)
+#SBATCH --ntasks=16 --nodes=1
+#SBATCH --time=06:00:00
+#SBATCH --job-name=pencen
+#SBATCH --output=slurm-%j.out
+module purge
+module load gaussian/g16
+/opt/software/scripts/job_prologue.sh
+export GAUSS_SCRDIR=$SLURM_SUBMIT_DIR
+g16 Molecule_pencen16cpu.gjf
+/opt/software/scripts/job_epilogue.sh
